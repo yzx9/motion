70e3496c3243c364459d298c07a9988c74a2188a
yzx9 <yuan.zx@outlook.com>
2023-11-07 22:35:47 +0800

    refactor(web-client/mine): change to tailwindcss

--- packages/web-client/src/views/Mine.vue
+++ packages/web-client/src/views/Mine.vue
@@ -13,35 +13,39 @@ async function updateData() {
 </script>
 
 <template>
-  <div class="container">
-    <div class="top">
-      <div class="avatar">
-        <img
-          src="http://npjy.oss-cn-beijing.aliyuncs.com/images/file-1575449277018pF3XL.jpg"
-        />
+  <div class="container w-screen h-screen text-white">
+    <div class="flex relative h-[40vh]">
+      <div>
+        <img class="my-4 mx-2 w-6 h-6 rounded-[50%]" :src="user.avatarURL" />
       </div>
 
-      <div class="detail">
-        <div class="nickname">{{ user.nickname }}</div>
+      <div class="my-4">
+        <div class="text-[0.8rem] font-bold">{{ user.nickname }}</div>
 
-        <div class="des">{{ user.des }}</div>
+        <div
+          class="w-[6.5rem] h-8 absolute left-[0.4rem] mt-[0.6rem] text-[0.4rem]"
+        >
+          {{ user.des }}
+        </div>
       </div>
 
-      <div class="wrap">
-        <div class="fan">
-          粉丝：<span class="number">{{ user.fan }}</span>
+      <div
+        class="absolute left-2 flex justify-around items-center mt-20 text-[0.5rem]"
+      >
+        <div class="ml-4 font-semibold">
+          粉丝：<span class="font-normal">{{ user.fan }}</span>
         </div>
 
-        <div class="follow">
-          关注：<span class="number">{{ user.follow }}</span>
+        <div class="font-semibold">
+          关注：<span class="font-normal">{{ user.follow }}</span>
         </div>
       </div>
     </div>
 
-    <div class="personal">
-      <div class="title">个人作品</div>
+    <div class="personal h-[60vh] rounded-[10%]">
+      <div class="title text-[0.7rem] text-center font-bold">个人作品</div>
 
-      <div class="video">
+      <div class="video h-full">
         <Grid :column-num="3" :gutter="5">
           <GridItem v-for="(item, index) in user.videos" :key="index">
             <img :src="item.img" />
@@ -54,77 +58,15 @@ async function updateData() {
 
 <style scoped>
 .container {
-  width: 100vw;
-  height: 100vh;
   /* background-color: #262626; */
   background: linear-gradient(to top, #575656, #262626);
-  color: #fff;
-}
-
-.top {
-  height: 40vh;
-  display: flex;
-  position: relative;
-}
-
-.avatar img {
-  margin: 1rem 0.5rem;
-  width: 1.5rem;
-  height: 1.5rem;
-  border-radius: 50%;
-}
-
-.detail {
-  margin: 1rem 0;
-}
-
-.nickname {
-  font-size: 0.8rem;
-  font-weight: 700;
-}
-
-.des {
-  position: absolute;
-  margin-top: 0.6rem;
-  font-size: 0.4rem;
-  left: 0.4rem;
-  width: 6.5rem;
-  height: 2rem;
-}
-
-.wrap {
-  position: absolute;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  left: 0.5rem;
-  font-size: 0.5rem;
-  margin-top: 5rem;
-}
-
-.fan {
-  font-weight: 600;
-  margin-right: 1rem;
-}
-
-.follow {
-  font-weight: 600;
-}
-
-.number {
-  font-weight: 500;
 }
 
 .personal {
-  height: 60vh;
   border-top: solid #fff;
-  border-radius: 10%;
 }
 
 .title {
-  text-align: center;
-  font-size: 0.7rem;
-  font-weight: 650;
   border-bottom: solid #474646;
 }
 
@@ -134,7 +76,6 @@ async function updateData() {
 }
 
 .video {
-  height: 100%;
   background-color: #474646 !important;
 }
 </style>
